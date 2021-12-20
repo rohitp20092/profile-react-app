38e94ca1edae9a8b69755fc50a2774dfe9599e85
rohitp20092 <rohitp20092@gmail.com>
2021-12-20 12:16:38 +0530

    fix:fixed editsubmit issue

--- src/container/EditProfile.js
+++ src/container/EditProfile.js
@@ -1,186 +1,133 @@
-import { FieldArray, Formik, ErrorMessage } from 'formik'
-import { Field } from 'formik-antd';
+import { ErrorMessage, FieldArray, Formik } from 'formik'
+import { Field, Form } from 'formik-antd'
 import React from 'react'
-import { Button } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
-import * as Yup from 'yup'
+import {  useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
+import { value } from '../action/action'
 
 function EditProfile() {
+  const dispatch = useDispatch()
+  const profile_data = useSelector(state => state)
   const Navigate = useNavigate()
-  var profileData = JSON.parse(localStorage.getItem('profiledata'))
-  const state1 = useSelector(state => state)
 
-  const validationSchema = Yup.object().shape({
-    firstname: Yup.string()
-      .min(2, "Too Short!")
-      .max(50, "Too Long!")
-      .required("Firstname is required"),
-
-    lastName: Yup.string()
-      .min(2, "Too Short!")
-      .max(50, "Too Long!")
-      .required("Lastname is required"),
-  });
-
+  console.log(profile_data)
   return (
-    <div className="inputprofile">
-
+    <div className="edit-profile">
       <Formik
         initialValues={{
-          firstname: profileData.firstname,
-          lastname: [profileData.lastname],
-          experience: state1[0]?.experience,
-          skills: state1[0]?.skills,
-          tagline:profileData.tagline
+          firstname: profile_data[0].firstname,
+          lastname: profile_data[0].lastname,
+          skills:profile_data[0].skills,
+          experience:profile_data[0].experience
         }}
-    
-        validationSchema={validationSchema}
         onSubmit={values => {
-          localStorage.setItem('profileData',values)
+          localStorage.setItem('profiledata', JSON.stringify(values))
+          dispatch(value(values))
           Navigate('/view-profile')
         }
         }>
-        {(props) => {
-          const {
-            values,
-            handleChange,
-            handleBlur,
-            handleSubmit
-          } = props;
-          localStorage.setItem('profiledata', JSON.stringify(values))
-
-          return (
-            <form onSubmit={handleSubmit} className="profile-input-form container mt-5">
-              <h1>Basic Details</h1>
-              <label>firstname:</label>
-              <input
-                type="text"
-                name="firstname"
-                placeholder={values.firstname}
-                value={values.firstname}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              <p style={{ color: "red" }}><ErrorMessage name="firstname" /></p>
-
-              <label>Last Name:</label>
-              <input
-                type="text"
-                name="lastname"
-                placeholder={values.lastname}
-                value={values.lastname}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              <p style={{ color: "red" }}><ErrorMessage name="lastname" /></p>
-
-              <label>Tagline:</label>
-              <input
-                type="text"
-                name="lastname"
-                placeholder={values.tagline}
-                value={values.tagline}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              <p style={{ color: "red" }}><ErrorMessage name="lastname" /></p>
-
-              <h1>Experience</h1>
-              <FieldArray
-                name="experience">
-                {arrayHelpers => (
-                  <div>
-                    {values.experience && values.experience.length > 0 ? (
-                      values.experience.map((friend, index) => (
-                        <div key={index}>
-                          <div>
-                            <label>company name</label>
-                            <Field name={`experience[${index}].company`} type="text" placeholder={values.experience[index].company} onChange={handleChange} />
-                          </div>
-                          <div>
-                            <label>title</label>
-                            <Field name={`experience[${index}].title`} type="text" onChange={handleChange} />
-                          </div>
-
-                          <div>
-                            <label>start date</label>
-                            <Field name={`experience[${index}].start_date`} type="date" onChange={handleChange} />
-                          </div>
-                          <div>
-                            <label>end_date</label>
-                            <Field name={`experience[${index}].end_date`} type="date" onChange={handleChange} />
-                          </div>
-                          
-                          <div>
-                            <label>desctiption</label>
-                            <Field name={`experience[${index}].description`} type="text" onChange={handleChange} />
-                            <p style={{ color: "red" }}><ErrorMessage name="description" /></p>
-
-                          </div>
-                          <button
-                            type="buttfriendson"
-                            onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
-                          >
-                            -
-                          </button>
-                          <button
-                            type="button"
-                            onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
-                          >
-                            +
-                          </button>
+        {({ values, handleSubmit, handleChange }) => (
+          <Form onSubmit={handleSubmit}>
+            <label>First Name:</label>
+            <Field type="text" name="firstname" value={values.firstname} onChange={handleChange} />
+            <button type="submit">submit</button>
+            <label>Last Name:</label>
+            <Field type="text" name="firstname" value={values.lastname} onChange={handleChange} />
+            <FieldArray
+              name="experience">
+              {arrayHelpers => (
+                <div>
+                  {values.experience && values.experience.length > 0 ? (
+                    values.experience.map((friend, index) => (
+                      <div key={index}>
+                        <div>
+                          <label>Company Name</label>
+                          <Field name={`experience[${index}].company`} type="text" />
                         </div>
-                      ))
-                    ) : (
-                      <Button type="button" onClick={() => arrayHelpers.push('')} className="success">
-                        {/* show this when user has removed all friends from the list */}
-                        Add Experience
-                      </Button>
-                    )}
-                    <div>
-                    </div>
-                  </div>
-                )}
-              </FieldArray>
-
-              <h3 className="my-2 ">Skills</h3>
-              <FieldArray
-                name="skills">
-                {arrayHelpers => (
-                  <div>
-                    {values.skills && values.skills.length > 0 ? (
-                      values.skills.map((skill, index) => (
-                        <div key={index}>
-                          <Field name={`skills[${index}]`} />
-                          <button
-                            type="button"
-                            onClick={() => arrayHelpers.remove(index)} // remove a skill from the list
-                          >
-                            -
-                          </button>
-                          <button
-                            type="button"
-                            onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
-                          >
-                            +
-                          </button>
+                        <div>
+                          <label>Title</label>
+                          <Field name={`experience[${index}].title`} type="text" />
                         </div>
-                      ))
-                    ) : (
-                      <Button type="button" onClick={() => arrayHelpers.push('')} className="primary">
-                        {/* show this when user has removed all skills from the list */}
-                        Add a skill
-                      </Button>
-                    )}
 
+                        <div>
+                          <label>Start Date</label>
+                          <Field name={`experience[${index}].start_date`} type="date" />
+                        </div>
+                        <div>
+                          <label>End Date</label>
+                          <Field name={`experience[${index}].end_date`} type="date" />
+                        </div>
+                        <div>
+                          <label>Desctiption</label>
+                          <Field name={`experience[${index}].description`} type="text" />
+                          <p style={{ color: "red" }}><ErrorMessage name={`experience[${index}].description`} /></p>
+
+                        </div>
+
+
+                        <button
+                          type="buttfriendson"
+                          onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                        >
+                          -
+                        </button>
+                        <button
+                          type="button"
+                          onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
+                        >
+                          +
+                        </button>
+                      </div>
+                    ))
+                  ) : (
+                    <button type="button" onClick={() => arrayHelpers.push('')} variant="primary">
+                      {/* show this when user has removed all friends from the list */}
+                      Add Experience
+                    </button>
+                  )}
+                  <div>
                   </div>
-                )}
-              </FieldArray>
-              <Button type="submit" className=" mt-5"  variant="success">Edit Submit</Button>
-            </form>
-          );
-        }}
+                </div>
+              )}
+            </FieldArray>
+            <FieldArray
+              name="skills">
+              {arrayHelpers => (
+                <div>
+                  {values.skills && values.skills.length > 0 ? (
+                    values.skills.map((skill, index) => (
+                      <div key={index}>
+                        <Field name={`skills.${index}`} />
+                        <button
+                          type="button"
+                          onClick={() => arrayHelpers.remove(index)} // remove a skill from the list
+                        >
+                          -
+                        </button>
+                        <button
+                          type="button"
+                          onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
+                        >
+                          +
+                        </button>
+                      </div>
+                    ))
+                  ) : (
+                    <button type="button" onClick={() => arrayHelpers.push('')} className="primary">
+                      {/* show this when user has removed all skills from the list */}
+                      Add a skill
+                    </button>
+                  )}
+
+                </div>
+              )}
+            </FieldArray>
+
+            <button type="submit">submit</button>
+          </Form>
+        )}
+
+
       </Formik>
 
     </div>
